60fcf853e60c1703b9e65e35d71669078aef6453
Matheus Abreu <matheusabreu.dev@gmail.com>
2020-09-29 17:46:27 +0100

    task(text): code lastChar util function

---
+++ utils/text.examples.ts
@@ -0,0 +1,4 @@
+import * as Text from "./text";
+
+const stringLastChar = Text.lastChar("js-smart-utils");
+console.log("The last char is: ", stringLastChar);
---
+++ utils/text.ts
@@ -0,0 +1,8 @@
+/**
+ * It returns the last char of a given
+ * text variable
+ * @param text string
+ */
+export function lastChar(text: string): string {
+  return text[text.length - 1];
+}
